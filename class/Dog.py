ea7d45aed530b19a5c8f1ebb93424fe38229082b
xiao-mianyang <joey.sin@qq.com>
2019-09-15 14:30:14 +0800

    commit class

---
+++ class/Dog.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+class Dog:
+    def __init__(self,name, age):
+        self.name = name
+        self.age = age
+
+    def __repr__(self):
+        return 'Dog:{}'.format(self.name)
+
+
+
+
+dog = Dog('Tom', 2)
+print(dog)
+print(dog.name)
+print(dog.age)
+
+
+
+
+
+
