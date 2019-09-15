0d9f0ee2d7a6154f50bdbc89c12c8614c8b81375
xiao-mianyang <joey.sin@qq.com>
2019-09-15 20:59:33 +0800

    updata classtest.py

--- class/classtest.py
+++ class/classtest.py
@@ -1,20 +1,30 @@
-#!/usr/bin/env python3
-
 class UserData:
-    def __init__(self, ID, Name):
+    def __init__(self, ID, name):
         self.ID = ID
-        self.Name = Name
-        print('ID:{} Name:{}'.format(self.ID, self.Name))
+        self._name = name
+
+    def __repr__(self):
+        return 'ID:{} Name:{}'.format(self.ID, self._name)
+
+
+class NewUser(UserData):
+    def get_name(self):
+        return self._name
     
-
-
-
+    def set_name(self, value):
+        self._name = value
+        
 
 if __name__ == '__main__':
-    user1 = UserData(101, 'Jack')
-    user2 = UserData(102, 'Louplus')
-    #print(user1)
-    #print(user2)
+    # 调用 NewUser 类继承的 UserData 类中的 __init__ 方法生成实例 user1 
+    user1 = NewUser(101, 'Jack')  
+    # 调用实例的 set_name 方法修改私有属性 _name
+    user1.set_name('Jackie')      
+    # 调用 NewUser 类继承的 UserData 类中的 __init__ 方法生成实例 user2
+    user2 = NewUser(102, 'Louplus')
+    # 打印实例，调用 __repr__ 方法
+    print(user1)
+    print(user2)
 
 
 
