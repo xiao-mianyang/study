67dc4e3a732207615ad78d4c870398c93c905174
xiao-mianyang <joey.sin@qq.com>
2019-09-17 00:27:17 +0800

    commit sumtest.py

---
+++ IO/sumtest.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+
+a = int(input('please enter the first num:'))
+b = int(input('please enter the second num:'))
+c = int(input('please enter the third num:'))
+e = a + b +c
+
+print('a is {}'.format(a),'b is {}'.format(b),'c is {}'.format(c),'a+b+c={}'.format(e))
+
+
+
+
+
