696976cc9981777b43fee20ebca635f8a7dc7a41
xiao-mianyang <joey.sin@qq.com>
2019-09-16 13:16:56 +0800

    updata class/classtest.py

--- class/classtest.py
+++ class/classtest.py
@@ -1,4 +1,7 @@
 class UserData:
+
+    group = 'shiyanlou-louplus'
+
     def __init__(self, ID, name):
         self.ID = ID
         self._name = name
@@ -13,18 +16,30 @@ class NewUser(UserData):
     
     def set_name(self, value):
         self._name = value
-        
+    
+    @classmethod
+    def get_group(cls):
+        return cls.group
 
+    @staticmethod
+    def format_userdata(user_id,user_name):
+        print('{}\'s id is {}'.format(user_name, user_id))
+
+
+        
 if __name__ == '__main__':
     # 调用 NewUser 类继承的 UserData 类中的 __init__ 方法生成实例 user1 
-    user1 = NewUser(101, 'Jack')  
+    #user1 = NewUser(101, 'Jack')  
     # 调用实例的 set_name 方法修改私有属性 _name
-    user1.set_name('Jackie')      
+    #user1.set_name('Jackie')      
     # 调用 NewUser 类继承的 UserData 类中的 __init__ 方法生成实例 user2
-    user2 = NewUser(102, 'Louplus')
+    #user2 = NewUser(102, 'Louplus')
     # 打印实例，调用 __repr__ 方法
-    print(user1)
-    print(user2)
+    #print(user1)
+    #print(user2)
+    print(NewUser.get_group())
+    NewUser.format_userdata(109,'Lucy')
+
 
 
 
