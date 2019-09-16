c5572461154b18db86a608b99381ae06b8df6d8d
xiao-mianyang <joey.sin@qq.com>
2019-09-16 22:02:31 +0800

    updata class/classtest.py

--- class/classtest.py
+++ class/classtest.py
@@ -6,16 +6,20 @@ class UserData:
         self.ID = ID
         self._name = name
 
-    def __repr__(self):
-        return 'ID:{} Name:{}'.format(self.ID, self._name)
+    #def __repr__(self):
+        #return 'ID:{} Name:{}'.format(self.ID, self._name)
 
 
 class NewUser(UserData):
-    def get_name(self):
+    @property
+    def name(self):
         return self._name
-    
-    def set_name(self, value):
-        self._name = value
+    @name.setter
+    def name(self, value):
+        if len(str(value))<=3:
+            print('ERROR')
+        else:
+            self._name = value
     
     @classmethod
     def get_group(cls):
@@ -24,6 +28,7 @@ class NewUser(UserData):
     @staticmethod
     def format_userdata(user_id,user_name):
         print('{}\'s id is {}'.format(user_name, user_id))
+    
 
 
         
@@ -37,9 +42,14 @@ if __name__ == '__main__':
     # 打印实例，调用 __repr__ 方法
     #print(user1)
     #print(user2)
-    print(NewUser.get_group())
-    NewUser.format_userdata(109,'Lucy')
-
+    #print(NewUser.get_group())
+    #NewUser.format_userdata(109,'Lucy')
+    user1 = NewUser(101, 'Jack')
+    user1.name = 'Lou'
+    user1.name = 'Jackie'
+    user2 = NewUser(102,'Louplus')
+    print(user1.name)
+    print(user2.name)
 
 
 
