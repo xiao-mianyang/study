104834bb4a6a93d24821c4d3f5b490f5c5f0b8a9
xiao-mianyang <joey.sin@qq.com>
2019-09-28 14:12:22 +0800

    在忽略文件中增加 Gedit 编辑器的备份文件

--- week3/app.py
+++ week3/app.py
@@ -1,6 +1,16 @@
-#!/usr/bin/env python3
-from flask import Flask
+from flask import Flask,render_template
+from flask import url_for
+from flask import redirect
+
 app = Flask(__name__)
+
+
+@app.route('/<username>')
+def hello(username):
+    if username =='shixiaolou':
+        return 'hello {}'.format(username)
+    else:
+        return redirect(url_for('index'))
 
 
 @app.route('/')
@@ -8,9 +18,38 @@ def index():
     return 'Hello World!'
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/user/<username>')
+def user_index(username):
+    return 'Hello {}'.format(username)
 
+
+@app.route('/courses/<coursesname>')
+def courses(coursesname):
+    #return render_template('courses.html,coursesname=coursesname')
+    return render_template('courses.html', coursesname=coursesname)
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post {}'.format(post_id)
+
+
+@app.route('/test')
+def test():
+    print(url_for('index'))
+    print(url_for('user_index',username='shiyanlou'))
+    print(url_for('show_post',post_id=1, _external=True))
+    print(url_for('show_post',post_id=2,q='python 03'))
+    print(url_for('show_post',post_id=2,q='python ok'))
+    print(url_for('show_post',post_id=2,_anchor='a'))
+    return 'test'
+
+
+
+
+
+if __name__ =='__main__':
+    app.run()
 
 
 
