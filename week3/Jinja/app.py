05611edd05f5422c69117b0fa970dd4051cde310
xiao-mianyang <joey.sin@qq.com>
2019-09-29 22:35:31 +0800

    commit app.py

---
+++ week3/Jinja/app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8 -*-
+
+from flask import Flask,render_template
+
+
+app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD']= True
+
+
+@app.route('/')
+def index():
+
+    teacher = {
+        'name':'Aiden',
+        'email':'loujin@simplecloud.cn'            
+    }
+   
+    course={
+        'mame':'Python Basic',
+        'teacher':teacher,
+        'user_count':5348,
+        'price':199.0,
+        'lab':None,
+        'is_private':False,
+        'is_member_course':True,       
+        'tags':['python','big data','Linux']
+    }
+  
+    return render_template('index.html',course=course)
+
+
+
+
+
