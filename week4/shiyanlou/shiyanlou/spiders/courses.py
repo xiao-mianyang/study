5dd81aaf8c326136ca317a8edf1254e1f67c541d
Joey0016 <joey.ziyi@qq.com>
2019-10-11 22:43:49 +0800

    updata spider

---
+++ week4/shiyanlou/shiyanlou/spiders/courses.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from shiyanlou.items import CourseItem
+
+
+class CoursesSpider(scrapy.Spider):
+    name = 'courses'
+    allowed_domains = ['shiyanlou.com']
+    
+    @property
+    def start_urls(self):
+        url_list = ['https://www.shiyanlou.com/courses/',
+                    'https://www.shiyanlou.com/courses/?page=2',
+                    'https://www.shiyanlou.com/courses/?page=3'
+                ]        
+
+        return url_list
+
+
+
+
+    def parse(self, response):
+        for course in response.css('div.col-sm-12.col-md-3'):
+            item = CourseItem({
+                    'name':course.css('h6.course-name::text').extract_first().strip(),
+                    'description':course.css('div.course-description::text').extract_first().strip(),
+                    'type':course.css('span.course-type::text').extract_first().strip(),
+                    'students':course.css('span.students-count span::text').extract_first()
+                    })
+
+            yield item
+
+
+
+
+
+
